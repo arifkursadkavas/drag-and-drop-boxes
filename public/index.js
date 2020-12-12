217216770205450a36b14499bb27f4d65d25e28e
hashgesh <arifkavas@gmail.com>
2020-12-12 13:38:25 +0100

    box merging

--- public/index.js
+++ public/index.js
@@ -4,6 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const numberOfBoxes = 5;
   let boxes = [];
 
+  let boxContainer = document.getElementById("boxContainer");
+
+  boxContainer.addEventListener("dragstart", handleContainerDragStart, false);
+  //boxContainer.addEventListener("dragover", handleDragOver, false);
+  //boxContainer.addEventListener("dragenter", handleDragEnter, false);
+  //boxContainer.addEventListener("dragleave", handleDragLeave, false);
+  boxContainer.addEventListener("dragend", handleContainerDragEnd, false);
+  boxContainer.addEventListener("drop", handleContainerDrop, false);
+
   class Node {
     constructor(id, sideLength, color, x, y, area, children, visible) {
       this.id = id;
@@ -38,11 +47,12 @@ document.addEventListener("DOMContentLoaded", function () {
       var box = generateBox(i);
       boxes.push(box);
     }
-    console.log(boxes);
+    //console.log(boxes);
     return boxes;
   }
 
   function applyBoxToHtml(box) {
+    //Set box text
     let nodeText = document.createElement("div");
     nodeText.innerHTML = box.area + " px²";
     nodeText.style.textAlign = "center";
@@ -50,9 +60,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let node = document.createElement("div");
     node.appendChild(nodeText);
-
+    node.setAttribute("id", box.id);
     node.className = "boxClass";
-
     node.style.position = "absolute";
     node.style.top = box.y;
     node.style.left = box.x;
@@ -60,15 +69,107 @@ document.addEventListener("DOMContentLoaded", function () {
     node.style.height = box.sideLength;
     node.style.backgroundColor = "#" + box.color;
     node.style.cursor = "move";
+    node.style.border = "solid 4px white";
+    node.style.borderRadius = "9px";
+
     node.setAttribute("draggable", true);
+
+    node.addEventListener("dragstart", handleDragStart, false);
+    node.addEventListener("dragover", handleDragOver, false);
+    node.addEventListener("dragenter", handleDragEnter, false);
+    node.addEventListener("dragleave", handleDragLeave, false);
+    node.addEventListener("dragend", handleDragEnd, false);
+    node.addEventListener("drop", handleDrop, false);
 
     document.getElementById("boxContainer").appendChild(node);
   }
 
+  //DRAG AND DROP UTILITIES
+  function handleDragStart(e) {
+    //console.log("handleDragStart", e, this);
+    this.style.opacity = "0.3";
+    this.style.borderStyle = "dashed";
+
+    e.dataTransfer.setData("boxId", this.getAttribute("id"));
+  }
+
+  function handleDragEnd(e) {
+    //console.log("handleDragEnd", e, this);
+    this.style.opacity = "1";
+    this.style.borderStyle = "solid";
+  }
+
+  function handleDragOver(e) {
+    //console.log("handleDragOver", e, this);
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    //console.log("handleDragEnter", e, this);
+    this.classList.add("over");
+  }
+
+  function handleDragLeave(e) {
+    //console.log("handleDragLeave", e, this);
+    this.classList.remove("over");
+  }
+
+  function handleDrop(e) {
+    //console.log("handleDrop", e, this);
+    e.stopPropagation();
+    //console.log("Drop data", e.dataTransfer.getData("boxId"));
+    const droppedBoxId = e.dataTransfer.getData("boxId");
+    const targetBoxId = this.getAttribute("id");
+
+    if (droppedBoxId === targetBoxId) return;
+
+    mergeBoxes(droppedBoxId, targetBoxId);
+
+    if (this.classList.contains("boxClass")) {
+      //console.log("Into the box");
+    } else {
+      //console.log("Out of the box");
+    }
+    return false;
+  }
+
+  function handleContainerDragStart(e) {
+    //console.log("handleContainerDragStart", this, e);
+    //e.dataTransfer.setData("boxId", this.getAttribute("id"));
+  }
+  function handleContainerDragEnd(e) {
+    if (e.target.className === "boxClass") {
+      const boxId = e.target.getAttribute("id");
+      //update store
+      const box = findBoxById(+boxId);
+
+      if (box) {
+        box.x = e.clientX - box.sideLength / 2 + 4;
+        box.y = e.clientY - box.sideLength / 2 + 4;
+        applyBoxesToHtml();
+      }
+    }
+    //console.log("Into the Container", e, this);
+  }
+  function handleContainerDrop(e) {
+    //console.log("handleContainerDrop", e, this);
+    e.stopPropagation();
+    //console.log("Drop data", e.dataTransfer.getData("boxId"));
+
+    return false;
+  }
+
   function applyBoxesToHtml() {
+    console.log(boxes);
     removeAllBoxesFromHTML();
     boxes.forEach(function (box) {
-      applyBoxToHtml(box);
+      if (box.visible) {
+        applyBoxToHtml(box);
+      }
     });
   }
 
@@ -104,6 +205,54 @@ document.addEventListener("DOMContentLoaded", function () {
     applyBoxesToHtml();
   }
 
+  function mergeBoxes(droppedBoxId, targetBoxId) {
+    const droppedBox = findBoxById(droppedBoxId);
+    const targetBox = findBoxById(targetBoxId);
+
+    droppedBox.visible = false;
+    targetBox.visible = false;
+
+    const newSideLength = droppedBox.sideLength + targetBox.sideLength;
+
+    const newNode = new Node(
+      boxes.length,
+      newSideLength,
+      getColor(droppedBox.color, targetBox.color),
+      targetBox.x,
+      targetBox.y,
+      newSideLength * newSideLength,
+      [droppedBox, targetBox],
+      true
+    );
+
+    boxes.push(newNode);
+
+    applyBoxesToHtml();
+  }
+
+  function getColor(color1, color2) {
+    const chunk1 = color1.match(/.{1,2}/g);
+    const chunk2 = color2.match(/.{1,2}/g);
+
+    let newColor = "";
+    for (var i = 0; i < chunk1.length; i++) {
+      const c1 = parseInt(chunk1[i], 16);
+      const c2 = parseInt(chunk2[i], 16);
+
+      const nc = Math.floor((c1 + c2) / 2);
+
+      newColor = newColor + nc.toString(16);
+    }
+
+    return newColor;
+  }
+
+  function findBoxById(id) {
+    return boxes.find(function (b) {
+      return b.id === +id;
+    });
+  }
+
   //Utility functions
   function removeAllBoxesFromHTML() {
     let currentBoxes = document.getElementsByClassName("boxClass");
@@ -112,8 +261,6 @@ document.addEventListener("DOMContentLoaded", function () {
       currentBoxes[0].remove();
     }
   }
-
-  function isOverLapping(boxes, x, y, sideLength) {}
 
   //generates a random integer in the min-max range
   function generateRandomInteger(min, max) {
@@ -131,8 +278,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const populateButton = document.getElementById("populate");
 
+  //Button handlers registered
   populateButton.addEventListener("click", function () {
-    //console.log("Button clicked");
     boxes = generateBoxes();
     applyBoxesToHtml(boxes);
   });
